d7d192d3906afe54082b074d3d0d62d00d0193ee
Adel Senhadji <adelsenhadjii@gmail.com>
2020-01-23 23:58:36 +0100

    added client API request

--- front/app.js
+++ front/app.js
@@ -1,15 +1,38 @@
-angular
-  .module("strong-pass", [])
-  .controller("spController", ["$http", function spController($http) {
-    this.numbers = false;
-    this.symbols = false;
+angular.module("strong-pass", []).controller("spController", [
+    "$http", "$scope",
+    function spController($http, $scope) {
+        this.length = 8
+        this.numbers = false;
+        this.symbols = false;
 
-    this.alarm = function() {
-      window.alert("hi");
-    };
+        this.alarm = function () {
+            window.alert("hi");
+        };
 
-    this.showStates = function(a, c1, c2) {
-      console.log(c1);
-      console.log(c2);
-    };
-  }]);
+        this.showStates = function (length, c1, c2) {
+            console.log(c1);
+            console.log(c2);
+
+            var request = {
+                length: length,
+                numbers: c1,
+                symbols: c2
+            }
+
+            $http({
+                method: "POST",
+                url: "http://localhost:8080/generate",
+                data: request,
+
+            }).then(function successCallback(response) {
+                console.log("()")
+                $scope.result = response.data.result
+            }, function errorCallback(response) {
+                console.log(response)
+            })
+
+
+
+        }
+    }
+]);
