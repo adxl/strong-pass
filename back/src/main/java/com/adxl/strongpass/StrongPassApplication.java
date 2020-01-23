1495dfe32aea57dc68dd5016a74df2f6b17c9f1c
Adel Senhadji <adelsenhadjii@gmail.com>
2020-01-23 13:19:21 +0100

    created project

---
+++ back/src/main/java/com/adxl/strongpass/StrongPassApplication.java
@@ -0,0 +1,15 @@
+package com.adxl.strongpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongPassApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StrongPassApplication.class, args);
+    }
+
+}
